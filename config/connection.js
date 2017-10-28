9fcd300883e6f896bd7e3ea83b2993a1f3522894
EdMacall <irvedwmac@aol.com>
2017-10-27 21:01:36 -0400

    fixed json package and connection.js for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "bosco12345",
-  database: "burgers_db"
-});
+if() 
+{
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else 
+{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "bosco12345",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
