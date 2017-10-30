4c8907ef32c82b7c2f657de229ae088415158350
EdMacall <irvedwmac@aol.com>
2017-10-30 12:20:31 -0400

    fixed bug which allows nameless burger to be created

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -26,22 +26,28 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newBurger = {
-      burger_name: $("#ca").val().trim(),
-      devoured: 0
-    };
+    var ca = $("#ca").val().trim();
+    if(ca !== "" && ca !== null)
+    {
+      var newBurger = {
+        burger_name: ca,
+        devoured: 0
+      };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          // console.log?  what for? 
+          // reloading the page will blank out the log
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    }
   });
 
   $(".delete-burger").on("click", function(event) {
